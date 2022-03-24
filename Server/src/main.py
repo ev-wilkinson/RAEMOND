4f479a12ac9a455cffe205c8a982d199b6d3f8db
Evan Wilkinson <ewilkinson49@outlook.com>
2022-03-23 21:52:05 -0600

    initial concept for button switching between states

--- Server/src/main.py
+++ Server/src/main.py
@@ -25,29 +25,41 @@ def main():
     data_log.init()
 
     while True:
-        s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-        s.bind((settings.SERVER_MAC_ADRRESS, settings.SERVER_PORT))
-        s.listen(settings.SERVER_BACKLOG)
-        client, _ = s.accept()
-        while True:
-            try:
-                recv_str = client.recv(settings.BUFFER_SIZE).decode().upper()
-                if recv_str:
-                    recv_str_list = recv_str.split()
-                    command = recv_str_list[0]
-                    if command in commands.command_dict.keys():
-                        try:
-                            args = recv_str_list[1:]
-                        except IndexError:
-                            args = []
-                        return_str = commands.command_dict[command](args)
-                        client.send(return_str.encode())
-                    else:
-                        client.send('Invalid Command!'.encode())
-            except ConnectionResetError:
-                client.close()
-                s.close()
-                break
+
+        # state with button enabled
+        while gpio.button_enabled:
+            gpio.IOCtrl.turn_off_blue() # turn off blue LED 
+            print('Motors running...')
+
+        # state with button disabled
+        while not gpio.button_enabled:
+            if gpio.BlueLEDBlinker.paused:
+                gpio.BlueLEDBlinker.resume() # blink blue LED    
+            s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM) # connect bluetooth
+            s.bind((settings.SERVER_MAC_ADRRESS, settings.SERVER_PORT))
+            s.listen(settings.SERVER_BACKLOG)
+            client, _ = s.accept()
+            gpio.BlueLEDBlinker.pause() # stop blinking blue LED  
+            gpio.IOCtrl.turn_on_blue() # keep blue LED on
+            while not gpio.button_enabled:
+                try:
+                    recv_str = client.recv(settings.BUFFER_SIZE).decode().upper()
+                    if recv_str:
+                        recv_str_list = recv_str.split()
+                        command = recv_str_list[0]
+                        if command in commands.command_dict.keys():
+                            try:
+                                args = recv_str_list[1:]
+                            except IndexError:
+                                args = []
+                            return_str = commands.command_dict[command](args)
+                            client.send(return_str.encode())
+                        else:
+                            client.send('Invalid Command!'.encode())
+                except ConnectionResetError:
+                    client.close()
+                    s.close()
+                    break
 
 if __name__=="__main__":
     main()
